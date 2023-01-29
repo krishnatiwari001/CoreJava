4e085bcccb1453f0c4db230de2036693e1c62df2
Krishna Tiwari <krishnatiwari001@gmail.com>
2023-01-30 01:52:46 +0530

    Checking

--- src/Others_4/Testing1.java
+++ src/Others_4/Testing1.java
@@ -2,7 +2,7 @@ package Others_4;
 
 public class Testing1 {
 	public static void main(String[] args) {
-		System.out.println("This is for testing for software");
+		System.out.println("This is for testing for softwarea");
 	}
 
 }
